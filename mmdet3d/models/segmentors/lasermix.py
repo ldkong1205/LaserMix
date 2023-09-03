5537211bf5fa5f3dd844db5c848b6f572d11ef4b
Lingdong Kong <ldkong@ieee.org>
2023-09-03 22:40:16 +0800

    Add files via upload

--- mmdet3d/models/segmentors/lasermix.py
+++ mmdet3d/models/segmentors/lasermix.py
@@ -17,14 +17,16 @@ from .semi_base import SemiBase3DSegmentor
 class LaserMix(SemiBase3DSegmentor):
 
     def __init__(self,
-                 segmentor: ConfigType,
+                 segmentor_student: ConfigType,
+                 segmentor_teacher: ConfigType,
                  semi_train_cfg: OptConfigType = None,
                  semi_test_cfg: OptConfigType = None,
                  loss_mse: OptConfigType = None,
                  data_preprocessor: OptConfigType = None,
                  init_cfg: OptMultiConfig = None) -> None:
         super(LaserMix, self).__init__(
-            segmentor=segmentor,
+            segmentor_student=segmentor_student,
+            segmentor_teacher=segmentor_teacher,
             semi_train_cfg=semi_train_cfg,
             semi_test_cfg=semi_test_cfg,
             data_preprocessor=data_preprocessor,
@@ -41,25 +43,16 @@ class LaserMix(SemiBase3DSegmentor):
         losses = dict()
 
         # sup loss
-        logits_sup_s, losses_sup = self.loss_by_gt_instances(
-            multi_batch_inputs['sup'], multi_batch_data_samples['sup'])
+        logits_sup_s, losses_sup = self.loss_by_gt_instances(multi_batch_inputs['sup'], multi_batch_data_samples['sup'])
         losses.update(**losses_sup)
 
         # generate pseudo instances for unlabeled data
-        logits_unsup_t, pseudo_data_samples = self.get_pseudo_instances(
-            multi_batch_inputs['unsup'], multi_batch_data_samples['unsup'])
+        logits_unsup_t, pseudo_data_samples = self.get_pseudo_instances(multi_batch_inputs['unsup'], multi_batch_data_samples['unsup'])
 
         # mt loss
         if self.loss_mse is not None:
-            logits_sup_t = self.teacher(
-                multi_batch_inputs['sup'],
-                multi_batch_data_samples['sup'],
-                mode='tensor')
-
-            logits_unsup_s = self.student(
-                multi_batch_inputs['unsup'],
-                multi_batch_data_samples['unsup'],
-                mode='tensor')
+            logits_sup_t = self.teacher(multi_batch_inputs['sup'], multi_batch_data_samples['sup'], mode='tensor')
+            logits_unsup_s = self.student(multi_batch_inputs['unsup'], multi_batch_data_samples['unsup'], mode='tensor')
 
             logits_s = torch.cat([logits_sup_s['logits'], logits_unsup_s['logits']])
             logits_t = torch.cat([logits_sup_t['logits'], logits_unsup_t['logits']])
@@ -163,5 +156,3 @@ class LaserMix(SemiBase3DSegmentor):
         labels_mix2 = torch.cat(labels_mix2)
 
         return points_mix1, points_mix2, labels_mix1, labels_mix2
-
-    # TODO: range view mix
